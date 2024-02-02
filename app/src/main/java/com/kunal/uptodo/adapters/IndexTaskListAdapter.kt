3970f1c27c1cafabb19327bb649a3474c31c5721
kunals1 <kunal.satpute@blackbuck.com>
2024-02-03 02:53:35 +0530

    format date

--- app/src/main/java/com/kunal/uptodo/adapters/IndexTaskListAdapter.kt
+++ app/src/main/java/com/kunal/uptodo/adapters/IndexTaskListAdapter.kt
@@ -5,6 +5,11 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.kunal.uptodo.databinding.ItemIndexTaskBinding
 import com.kunal.uptodo.models.NewTaskModel
+import java.text.SimpleDateFormat
+import java.time.ZoneId
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 
 class IndexTaskListAdapter(private val onTaskDeleteClick : ((Long) -> Unit)?) : RecyclerView.Adapter<IndexTaskListAdapter.IndexTaskListViewHolder>() {
@@ -16,7 +21,7 @@ class IndexTaskListAdapter(private val onTaskDeleteClick : ((Long) -> Unit)?) : 
         RecyclerView.ViewHolder(binding.root) {
         fun bind(newTask: NewTaskModel) = binding.run {
             tvTaskName.text = newTask.title
-            tvDeadline.text = newTask.deadlineDate.toString()
+            tvDeadline.text = newTask.deadlineDate?.let { formatDate(it) }
             radioButton.isChecked = false
             radioButton.setOnClickListener{
                 onTaskDeleteClick?.invoke(newTask.timeCreated)
@@ -42,4 +47,32 @@ class IndexTaskListAdapter(private val onTaskDeleteClick : ((Long) -> Unit)?) : 
         this.taskList = taskList
         notifyDataSetChanged() //todo optimized this later
     }
+
+    private fun formatDate(longDate: Long): String {
+        val dateObject = Date(longDate)
+        val currentDate = Date()
+
+        val deadlineTime = SimpleDateFormat("HH:mm", Locale.getDefault())
+
+        // Format date and time
+        val formattedDateTime =
+            SimpleDateFormat("dd-MMM", Locale.getDefault()).format(dateObject)
+
+        // Get day part of the date
+        val currentDay =
+            Calendar.getInstance().apply { time = currentDate }.get(Calendar.DAY_OF_YEAR)
+        val targetDay = Calendar.getInstance().apply { time = dateObject }.get(Calendar.DAY_OF_YEAR)
+
+        // Check if the date is today
+        return if (currentDay == targetDay) {
+            "Today At ${deadlineTime.format(dateObject)}"
+        }
+        // Check if the date is tomorrow
+        else if (currentDay + 1 == targetDay) {
+            "Tomorrow At ${deadlineTime.format(dateObject)}"
+        } else {
+            "$formattedDateTime At ${deadlineTime.format(dateObject)}"
+        }
+    }
+
 }
